c379f611881c713e11e79a40fade9294e8b67a5e
Zhao <zhaobruce@ad.garmin.com>
2018-10-10 16:58:52 +0800

    read gpx and txt

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(249, 25);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 25);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(30, 157);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(30, 193);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(216, 21);
+            this.textBox3.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(128, 225);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 22);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Start";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(258, 155);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 22);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "gpx file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(258, 193);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 22);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "txt file";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 257);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //public void 
+#if (false)
+            public void gauss_solve(int n, double[] A, double[] x, double[] b)
+        {
+            int i, j, k, r; double max;
+            for (k = 0; k < n - 1; k++)
+            {
+                max = fabs(A[k * n + k]); /*find maxmum*/
+                r = k;
+                for (i = k + 1; i < n - 1; i++)
+                    if (max < fabs(A[i * n + i]))
+                    { max = fabs(A[i * n + i]); r = i; }
+                if (r != k) for (i = 0; i < n; i++)         /*change array:A[k]&A[r] */
+                    {
+                        max = A[k * n + i]; A[k * n + i] = A[r * n + i]; A[r * n + i] = max;
+                    }
+                max = b[k];                    /*change array:b[k]&b[r]     */
+                b[k] = b[r]; b[r] = max;
+                for (i = k + 1; i < n; i++)
+                for (i = k + 1; i < n; i++)
+                for (i = k + 1; i < n; i++)
+                for (i = k + 1; i < n; i++)
+                {
+                    for (j = k + 1; j < n; j++)
+                        A[i * n + j] -= A[i * n + k] * A[k * n + j] / A[k * n + k];
+                    b[i] -= A[i * n + k] * b[k] / A[k * n + k];
+                }
+            }
+            for (i = n - 1; i >= 0; x[i] /= A[i * n + i], i--)
+                for (j = i + 1, x[i] = b[i]; j < n; j++)
+                    x[i] -= A[i * n + j] * x[j];
+        }
+#endif
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string waypoint_info = null;
+            string file_name = null;
+            string waypoint_trkpt = null;
+            string waypoint_time = null;
+            string waypoint_ele = null;
+            bool new_file_is_creat = false;
+            OpenFileDialog dialog  = new OpenFileDialog();
+            dialog.Filter = "gpx文件|*.gpx";
+            if (dialog.ShowDialog() == DialogResult.OK && null != dialog.FileName)
+            {
+                textBox1.Text = dialog.FileName;
+            }
+            else
+            {
+                MessageBox.Show("文件路径无效！");
+                return;
+            }
+
+            string new_file_path = Path.GetDirectoryName(dialog.FileName) + "\\" + Path.GetFileNameWithoutExtension(dialog.FileName) + ".txt";
+            FileStream fs = new FileStream( new_file_path,FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter m_streamWriter = new StreamWriter(fs);
+            m_streamWriter.Flush();
+            m_streamWriter.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            XmlTextReader readerXml = new XmlTextReader(dialog.FileName);
+            while (readerXml.Read())
+            {
+                if (readerXml.NodeType == XmlNodeType.Element)
+                {
+                    if ("trkpt" == readerXml.Name )
+                    {
+                        waypoint_trkpt = readerXml.GetAttribute("lat") + "\t" + readerXml.GetAttribute("lon");
+                    }
+                    else if("ele" == readerXml.Name )
+                    {
+                        waypoint_ele = readerXml.ReadString().Trim();
+                    }
+                    else if ("time" == readerXml.Name)
+                    {
+                        waypoint_time = readerXml.ReadString().Trim();
+                        if( waypoint_time != null && waypoint_trkpt != null && waypoint_ele != null)
+                        {
+                            waypoint_info = waypoint_time + "\t" + waypoint_trkpt + "\t" + waypoint_ele + "\n";
+                            m_streamWriter.Write(waypoint_info);
+                        }
+                    }
+                }
+            }
+            readerXml.Close();
+            m_streamWriter.Flush();
+            m_streamWriter.Close();
+            MessageBox.Show("处理完毕！");
+            System.Diagnostics.Process.Start(new_file_path);
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "txt文件|*.txt";
+            if (dialog.ShowDialog() == DialogResult.OK && null != dialog.FileName)
+            {
+                textBox3.Text = dialog.FileName;
+            }
+            else
+            {
+                MessageBox.Show("文件路径无效！");
+                return;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "gpx文件|*.gpx";
+            if (dialog.ShowDialog() == DialogResult.OK && null != dialog.FileName)
+            {
+                textBox2.Text = dialog.FileName;
+            }
+            else
+            {
+                MessageBox.Show("文件路径无效！");
+                return;
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string waypoint_trkpt_lat = null;
+            string waypoint_trkpt_lon = null;
+            string[] txt_line_string_arry = new string[4];
+            string txt_line_string ;
+            StreamReader fs = new StreamReader(textBox3.Text, Encoding.Default);
+            string new_file_path = Path.GetDirectoryName(textBox2.Text) + "\\" + Path.GetFileNameWithoutExtension(textBox2.Text) + "_new.gpx";
+            //StreamReader m_streamWriter = new StreamReader(fs);
+            XmlTextWriter writeXml = new XmlTextWriter(new_file_path, Encoding.UTF8);
+            writeXml.WriteStartDocument(false);
+            //writeXml.WriteStartElement("?xml version="1.0" encoding="UTF - 8" standalone="no" ?");
+
+            // writeXml.WriteStartElement( xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensions/v3/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtension/v1/TrackPointExtensionv1.xsd");
+            writeXml.WriteWhitespace("\n");
+            writeXml.WriteStartElement("gpx");
+            writeXml.WriteAttributeString("xmlns", "http://www.topografix.com/GPX/1/1");
+            writeXml.WriteAttributeString("xmlns:gpxx", "http://www.garmin.com/xmlschemas/GpxExtensions/v3");
+            writeXml.WriteAttributeString("xmlns:gpxtpx", "http://www.garmin.com/xmlschemas/TrackPointExtension/v1");
+            writeXml.WriteAttributeString("creator", "Garmin Colorado");
+            writeXml.WriteAttributeString("version", "1.1");
+            writeXml.WriteAttributeString("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+            writeXml.WriteAttributeString("xsi:schemaLocation", "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensions/v3/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtension/v1/TrackPointExtensionv1.xsd");
+
+            writeXml.WriteWhitespace("\n");
+            writeXml.WriteWhitespace(" ");
+            writeXml.WriteStartElement("trk");
+            writeXml.WriteWhitespace("\n");
+            writeXml.WriteWhitespace("  ");
+            writeXml.WriteElementString("name", Path.GetFileNameWithoutExtension(textBox2.Text) + "_new");
+            writeXml.WriteWhitespace("\n");
+            XmlTextReader readerXml = new XmlTextReader(textBox2.Text);
+
+            while (readerXml.Read())
+            {
+                if (readerXml.NodeType == XmlNodeType.Element )
+                {
+                    if("desc" == readerXml.Name)
+                    {
+                        writeXml.WriteWhitespace("  ");
+                        writeXml.WriteElementString("desc", readerXml.ReadString().Trim());
+                        writeXml.WriteWhitespace("\n");
+                        writeXml.WriteWhitespace("  ");
+                        writeXml.WriteStartElement("trkseg");
+                        writeXml.WriteWhitespace("\n");
+                    }
+                    if ("trkpt" == readerXml.Name)
+                    {
+                        txt_line_string = fs.ReadLine();
+                        txt_line_string_arry = txt_line_string.Split('\t');
+                        writeXml.WriteWhitespace("   ");
+                        writeXml.WriteStartElement("trkpt");
+                        writeXml.WriteAttributeString("lat", txt_line_string_arry[1]);
+                        writeXml.WriteAttributeString("lon", txt_line_string_arry[2]);
+                        writeXml.WriteWhitespace("\n");
+                    }
+                    else if ("ele" == readerXml.Name)
+                    {
+                        writeXml.WriteWhitespace("    ");
+                        writeXml.WriteElementString("ele", txt_line_string_arry[3]);
+                        writeXml.WriteWhitespace("\n");
+                    }
+                    else if ("time" == readerXml.Name)
+                    {
+                        writeXml.WriteWhitespace("    ");
+                        writeXml.WriteElementString("time", txt_line_string_arry[0]);
+                        writeXml.WriteWhitespace("\n");
+                        writeXml.WriteWhitespace("   ");
+                        writeXml.WriteEndElement();//trkpt
+                        writeXml.WriteWhitespace("\n");
+                    }
+                }
+            }
+            writeXml.WriteWhitespace("  ");
+            writeXml.WriteEndElement();//trkseg
+            writeXml.WriteWhitespace("\n");
+            writeXml.WriteWhitespace(" ");
+            writeXml.WriteEndElement();//trk
+            writeXml.WriteWhitespace("\n");
+            writeXml.WriteEndElement();//gpx
+            //writeXml.WriteEndElement();
+
+
+            writeXml.Flush();
+            writeXml.Close();
+
+            readerXml.Close();
+            fs.Close();
+
+            //m_streamWriter.Flush();
+           // m_streamWriter.Close();
+            MessageBox.Show("处理完毕！");
+            System.Diagnostics.Process.Start(new_file_path);
+        }
+
+
+    }
+}
